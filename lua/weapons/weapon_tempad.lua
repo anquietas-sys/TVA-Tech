33605f2db9446a4b1441865c2a1a73310407b077
Virgil <cadenbennett3@gmail.com>
2025-06-15 15:20:09 -0500

    Started work on the Tempad

---
+++ lua/weapons/weapon_tempad.lua
@@ -0,0 +1,24 @@
+SWEP.PrintName			= "Tempad" -- This will be shown in the spawn menu, and in the weapon selection menu
+SWEP.Author			= "Time Variants Authority" -- These two options will be shown when you have the weapon highlighted in the weapon selection menu
+SWEP.Instructions		= "Left Mouse to open a Time Door, Right mouse to close it. Reload to open menu."
+SWEP.Spawnable = true
+SWEP.AdminOnly = false
+SWEP.Primary.ClipSize		= -1
+SWEP.Primary.DefaultClip	= -1
+SWEP.Primary.Automatic		= true
+SWEP.Primary.Ammo		= "none"
+
+SWEP.Secondary.ClipSize		= -1
+SWEP.Secondary.DefaultClip	= -1
+SWEP.Secondary.Automatic	= false
+SWEP.Secondary.Ammo		= "none"
+SWEP.Weight			= 5
+SWEP.AutoSwitchTo		= false
+SWEP.AutoSwitchFrom		= false
+
+SWEP.Slot			= 1
+SWEP.SlotPos			= 2
+SWEP.DrawAmmo			= false
+SWEP.DrawCrosshair		= true
+SWEP.ViewModel			= "models/weapons/c_medkit.mdl"
+SWEP.WorldModel			= "models/items/healthkit.mdl"
