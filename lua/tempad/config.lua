afdee816f972935689069a5ae65890360076d4a1
Virgil <cadenbennett3@gmail.com>
2025-06-15 13:23:48 -0500

    Update config.lua

---
+++ lua/tempad/config.lua
@@ -0,0 +1,12 @@
+local config = {} // Don't Touch
+
+config.Blacklist = { // 
+    "func_door" = true,
+    "func_door_rotating" = true,
+    
+    "prop_door" = true,
+    "prop_door_rotating" = true,
+}
+
+
+return config // Don't Touch
