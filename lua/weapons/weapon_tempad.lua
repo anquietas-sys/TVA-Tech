6acd9cfc672250f56a39edd584c4953e7ffaf2ee
Virgil <cadenbennett3@gmail.com>
2025-06-15 15:59:03 -0500

    Why Won't This Goddamn Derma Panel Work
    help me pls

--- lua/weapons/weapon_tempad.lua
+++ lua/weapons/weapon_tempad.lua
@@ -1,6 +1,7 @@
-SWEP.PrintName			= "Tempad" -- This will be shown in the spawn menu, and in the weapon selection menu
-SWEP.Author			= "Time Variants Authority" -- These two options will be shown when you have the weapon highlighted in the weapon selection menu
-SWEP.Instructions		= "Left Mouse to open a Time Door, Right mouse to close it. Reload to open menu."
+SWEP.PrintName			= "Tempad"
+SWEP.Author			= "Time Variance Authority"
+SWEP.Instructions		= "Left Mouse to open a Time Door, Right mouse to open menu."
+SWEP.Category = "Time Variance Authority"
 SWEP.Spawnable = true
 SWEP.AdminOnly = false
 SWEP.Primary.ClipSize		= -1
@@ -16,9 +17,44 @@ SWEP.Weight			= 5
 SWEP.AutoSwitchTo		= false
 SWEP.AutoSwitchFrom		= false
 
-SWEP.Slot			= 1
-SWEP.SlotPos			= 2
+SWEP.Slot			= 5
+SWEP.SlotPos			= 3
 SWEP.DrawAmmo			= false
 SWEP.DrawCrosshair		= true
 SWEP.ViewModel			= "models/weapons/c_medkit.mdl"
 SWEP.WorldModel			= "models/items/healthkit.mdl"
+SWEP.ViewModelFOV = 54
+SWEP.UseHands = true
+SWEP.ShootSound = Sound( "buttons/button15.wav" )
+
+local function OpenMenu()
+
+	local myTitleLabel = vgui.Create( "DLabel", myBasePanel )
+	myTitleLabel:SetText( "My Awesome Panel" )
+	myTitleLabel:SetPos( 10, 10 )
+	myTitleLabel:SizeToContents()
+	myTitleLabel:SetTextColor( color_white )
+
+	local myCloseButton = vgui.Create( "DButton", myBasePanel )
+	myCloseButton:SetText( "Close Me" )
+	myCloseButton:SetSize( 100, 30 )
+	local panelW, panelH = myBasePanel:GetSize()
+	myCloseButton:SetPos( panelW - 110, panelH - 40 )
+
+	myCloseButton.DoClick = function( theButton )
+	  print( "Close button clicked!" )
+	  myBasePanel:Remove()
+	end
+end
+
+
+
+function SWEP:PrimaryAttack()
+	self:SetNextPrimaryFire( CurTime() + 0.5 )
+	print("test")
+end
+
+function SWEP:SecondaryAttack()
+	self:SetNextPrimaryFire( CurTime() + 0.5 )
+	OpenMenu()
+end
