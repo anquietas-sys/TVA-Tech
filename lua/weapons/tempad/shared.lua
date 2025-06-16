9fcad02a7ef755c2a1e34d84c3bb9dfdd295dbea
Virgil <cadenbennett3@gmail.com>
2025-06-16 14:57:48 -0500

    Update how the tempad works.
    Added closing individual or pairs of doors.
    Swapped the binds around because I felt like it.

--- lua/weapons/tempad/shared.lua
+++ lua/weapons/tempad/shared.lua
@@ -1,6 +1,6 @@
 SWEP.PrintName			= "Tempad"
 SWEP.Author			= "Time Variance Authority"
-SWEP.Instructions		= "Left Mouse to open a Time Door, Right mouse to open menu."
+SWEP.Instructions		= "Left Mouse to open the menu, Right mouse to close a Time Door."
 SWEP.Category = "Time Variance Authority"
 SWEP.Spawnable = true
 SWEP.AdminOnly = false
@@ -28,15 +28,28 @@ SWEP.UseHands = true
 SWEP.ShootSound = Sound( "buttons/button15.wav" )
 
 // This following code will run on both client AND server
-function SWEP:PrimaryAttack()
-	if ( game.SinglePlayer() ) then self:CallOnClient( "PrimaryAttack" ) end
-	self:SetNextPrimaryFire( CurTime() + 0.5 )
-
-	// Do shit here idk
-end
-
 function SWEP:SecondaryAttack()
 	if ( game.SinglePlayer() ) then self:CallOnClient( "SecondaryAttack" ) end
+	self:SetNextSecondaryFire( CurTime() + 0.5 )
+
+	if SERVER then
+		local owner = self:GetOwner()
+
+		local tr = owner:GetEyeTrace()
+		local targetEnt = tr.Entity
+
+		if IsValid(targetEnt) and targetEnt:GetClass() == "timedoor" or targetEnt:GetClass() == "timedoorglitchy" then
+    	local ptnr = targetEnt.Partner
+    	if IsValid(ptnr) then
+    		ptnr:CloseDoor(true)
+    	end
+    	targetEnt:CloseDoor(true)
+		end
+	end
+end
+
+function SWEP:PrimaryAttack()
+	if ( game.SinglePlayer() ) then self:CallOnClient( "PrimaryAttack" ) end
 	self:SetNextPrimaryFire( CurTime() + 0.5 )
 	if CLIENT then
 		self:OpenMenu()
@@ -66,6 +79,15 @@ if SERVER then
 	end)
 end
 
+if CLIENT then
+	function SWEP:CloseDoor()
+
+	end
+end
+
+
+
+
 // Will only run on the client. I put this at the bottom because clientside code (especially vgui stuff) takes a lot of space;
 //    Space I doubt you want to wade through to get to literally any other code haha	
 if CLIENT then
@@ -74,37 +96,14 @@ if CLIENT then
 		frame:SetSize(ScrW()/2, ScrH()/2) // Set the frame size to half of the screen's width and height
 		frame:Center() // Centers the frame on the screen
 		frame:MakePopup() // Unlocks the mouse, shows the cursor so we can interact with the panel
-		frame:SetTitle("Time Variance Authority :: Controls") // Sets the title of the frame
+		frame:SetTitle("Tempad") // Sets the title of the frame
 
 		// doin a heckin advanced!1!! (use this to set the background color of the frame)
 		function frame:Paint(w,h)
 			draw.RoundedBox(8, 0, 0, w, h, Color(25,25,25,250))
 		end
 
-		// HTML is so much better, although it requires the x86-64 branch to use properly.
-		local html = vgui.Create("DHTML", frame)
-		html:Dock(FILL) // Just makes the HTML the full size of the frame
-
-		// Coding languages within coding languages are always messy though.
-		// In my opinion, this is better than making 5 million label elements haha
-		html:SetHTML([[
-			<h1>This is my HTML Code!</h1>
-
-			<p>HTML is a lot easier to create with and style than whatever the fuck derma has you do.</p>
-			<p style="color: red; font-family: Arial; font-weight: bold;">That being said, it's non-interactive. You *can* create buttons in HTML, but there's no decent way to move the input of the button out of HTML and into LUA.</p>
-		]]..[[
-			<style>
-				body {
-					margins: 0;
-					padding: 0;
-					font-family: Impact;
-					text-align: center;
-					color: #fff;
-					background-color: #333a;
-				}
-			</style>
-		]])
-
+		
 		local networked = vgui.Create("DButton", frame)
 		networked:SetHeight(frame:GetTall()/10) // Size everything relative to the frame to keep support for all monitor resolutions.
 		networked:Dock(BOTTOM) // Fills the bottom spot with the button
